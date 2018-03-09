44a536a4ecf80a6ef61e2ab3ee3b622151c25963
Hubertus <Hubertus.Bergmann@web.de>
2018-03-09 07:26:03 -0800

    flash_defconfig as default

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_KERNEL_CMDLINE += raid=noautodetect
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := clang-stable
 TARGET_KERNEL_SOURCE := kernel/google/taimen
-TARGET_KERNEL_CONFIG := custom_defconfig
+TARGET_KERNEL_CONFIG := flash_defconfig
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 
